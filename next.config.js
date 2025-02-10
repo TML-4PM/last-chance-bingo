dfc4dadd0030514120df705d02cec2a5079ee867
TML-4PM <62584578+TML-4PM@users.noreply.github.com>
2025-02-10 20:37:15 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development'
 });
+
 module.exports = withPWA({
-  reactStrictMode: true
+  reactStrictMode: true,
+  eslint: {
+    // This option disables ESLint errors during production builds.
+    ignoreDuringBuilds: true,
+  }
 });
